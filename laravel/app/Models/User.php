41bd669b31f5ebb94ed3497a8c6f13efea19348b
takumiiimo <assistant2370@gmail.com>
2020-10-25 22:18:14 +0900

    ユニットテストの作り方を学ぶ #2

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -36,4 +38,35 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function reseervations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function reservationCountThisMonth(): int
+    {
+        $today = Carbon::today();
+        return $this->reservations()
+            ->whereYear('created_at', $today->year)
+            ->whereMonth('created_at', $today->month)
+            ->count();
+    }
+
+    /** 
+     * @param Lesson $lesson
+     * @return void
+     * @throws \Exception
+     */
+    public function canReserve(Lesson $lesson): void
+    {
+        if($lesson->remainingCount() === 0){
+            throw new \Exception('レッスンの予約可能上限に達しています。');
+        }
+        if($this->plan === 'gold'){
+            return;
+        }
+        if($this->reservationCountThisMonth() === 5){
+            throw new \Exception('今月の予約がプランの上限に達しています。');
+        }
+    }
 }
